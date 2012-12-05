1b3be0ca49ae8137469e3be0bf4bf458cf1fb98f
Jasper Poppe <jpoppe@ebay.com>
2012-12-05 16:57:43 +0100

    minor cleanup

--- seedbank/seedbank.py
+++ seedbank/seedbank.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 # Copyright 2009-2012 Jasper Poppe <jgpoppe@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -207,13 +207,8 @@ def argument_parser():
 
 def main():
     """the main application"""
-
     try:
-        #if sys.stdin.isatty():
         argument_parser()
-        #else:
-        #    piped = sys.stdin.read()
-        #    print(piped)
     except utils.FatalException:
         sys.exit(1)
     except Exception as err:
