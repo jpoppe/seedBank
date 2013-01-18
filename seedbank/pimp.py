10bf0a04e6c326cbf08e504fde2d95eda4dd142d
Jasper Poppe <jpoppe@ebuddy.com>
2013-01-18 16:31:16 +0100

    fixed white spaces, empty lines

--- seedbank/pimp.py
+++ seedbank/pimp.py
@@ -1,13 +1,13 @@
 """this module processes the preseed templates and file overlays """
 
 # Copyright 2009-2012 Jasper Poppe <jgpoppe@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -187,8 +187,8 @@ class OverlayPermissions:
             for real_path in perm_list:
                 if real_path in defined_list:
                     perm_list[real_path] = defined_list[real_path]
-       
-        data = [] 
+
+        data = []
         header_file = os.path.join(self.cfg['paths']['templates'],
             self.cfg['templates']['permission_script'])
         header = utils.file_read(header_file)
